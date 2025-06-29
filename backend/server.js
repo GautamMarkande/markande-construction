4f38665bdf1265b33082071c6372c35d246ce7a1
gautammarkande <gautammarkande71@gmail.com>
2025-06-29 16:17:24 +0530

    addded server.json

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+//import packages
+const express = require("express");
+const cors = require("cors");
+//db connection file imported
+const db = require("../backend/db");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const MongoDbStore = require("connect-mongodb-session")(session);
+const path = require("path");
+const port = parseInt(process.env.PORT) || 3000;
+
+const _dirname = path.resolve();
+const app = express();
+app.use(cors());
+
+//files importing
+const UserRouter = require("../backend/Controllers/UserController");
+const ListingRouter = require("../backend/Controllers/ListingController");
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+Routers
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Construction App API");
+});
+app.use("/api/user", UserRouter);
+app.use("/api/listing", ListingRouter);
+app.listen(process.env.PORT, () => {
+  console.log("server is running on 3000");
+});
